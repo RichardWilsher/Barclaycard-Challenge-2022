5dfcd77f48e16267597de6da9b333bf23f2f9fa9
abbymcinerney <abby.mcinerney@outlook.com>
2022-02-08 10:59:49 +0000

    about grid

--- templates/about.html.php
+++ templates/about.html.php
@@ -1,2 +1,10 @@
-<h1>About Us</h1>
+<h2>About Us</h2>
 <p>Welcome to the Barclaycard challenge 2022</p>
+<div class="grid">
+
+<img class="grid-item" src="" alt="">
+<img class="grid-item" src="" alt="">
+<img class="grid-item" src="" alt="">
+<img class="grid-item" src="" alt="">
+<img class="grid-item" src="" alt="">
+</div>
